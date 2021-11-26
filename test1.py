ef30b754a5a0c3adf10fa5149814ce3bde8f908a
Unyong Park <qkrdns00@gmail.com>
2021-11-26 22:56:26 +0900

    first commit

---
+++ test1.py
@@ -0,0 +1,28 @@
+number = 10
+day ="three"
+print("I ate %d apples. so I was sick for %s days" %(number,day))\
+    
+print("I have %s apples" %3)
+print("rate is %s" %3.234)
+print("Error is %d%%" % 98)
+print("%10s" % 'hi')
+print("%10.4f" %3.421342343)
+print("I eat {0} apples".format(3))
+print("I eat {0} apples".format("five"))
+print("I ate {a} apples. so I was sick for {b} days".format(a=10,b="five"))
+
+print("{0:=^10}".format("hi"))
+
+name = '홍길동'
+age = 30
+print(f"나의 이름은 {name} 입니다. 나이는 {age+1} 입니다.")
+
+b=name.find('홍')
+print(b)
+print(",".join('abcd'))
+
+
+c = "Life is too short"
+print(c.replace("Life","Your leg"))
+
+
